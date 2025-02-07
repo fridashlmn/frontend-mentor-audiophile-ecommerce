230e0d89bc247c21bea5ca3c377383c06ae41b91
Frida <friederike.suhlmann@gmail.com>
2025-02-07 13:42:59 +0100

    feat: activate nuxt content studio

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,17 +3,28 @@ import tailwindcss from '@tailwindcss/vite'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxt/content', '@nuxt/eslint'],
+
+  content: {
+    preview: {
+      api: 'https://api.nuxt.studio',
+    },
+  },
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   nitro: {
     srcDir: '/app',
     static: true,
     preset: 'netlify-static',
   },
+
   devtools: { enabled: true },
+
   future: {
     compatibilityVersion: 4,
   },
+
   compatibilityDate: '2024-04-03',
 })
